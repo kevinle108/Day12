7a6187f5d05c712c2fedbc937b22c155d38910a7
Kevin Le <kevinle108@gmail.com>
2021-03-11 10:16:50 -0500

    done 12b

--- Program.cs
+++ Program.cs
@@ -8,8 +8,8 @@ namespace Day12
         static void Main(string[] args)
         {
             var ls = new List<string> { "Ant", "Bat", "Cat", "Dog" };
-            int k = 3;
-            KGroupAssignments(ls, k);
+            int k = 2;
+            KGroupAssignmentsEmpty(ls, k);
         }
 
         static void GroupAssignments(List<string> ls)
@@ -78,6 +78,59 @@ namespace Day12
             KGroupAssignments(ls, idx + 1, groups, k);
             groups.RemoveAt(groups.Count - 1);
         }
+        
+        static void KGroupAssignmentsEmpty(List<string> ls, int k)
+        {
+            var groups = new List<List<string>>();
+            KGroupAssignmentsEmpty(ls, 0, groups, k);
+        }
+
+        static void KGroupAssignmentsEmpty(List<string> ls, int idx, List<List<string>> groups, int k)
+        {
+            if (ls.Count == idx)
+            {
+                if (groups.Count <= k) 
+                {
+                    PrintGroupsEmpty(groups, k);
+                }
+                return;
+            }
+            // Add the current element to each existing group
+            for (int i = 0; i < groups.Count; i++)
+            {
+                groups[i].Add(ls[idx]);
+                KGroupAssignmentsEmpty(ls, idx + 1, groups, k);
+                groups[i].Remove(ls[idx]);
+            }
+            // Add the current element to a new group
+            groups.Add(new List<string>());
+            groups[groups.Count - 1].Add(ls[idx]);
+            KGroupAssignmentsEmpty(ls, idx + 1, groups, k);
+            groups.RemoveAt(groups.Count - 1);
+        }
+
+        static void PrintGroupsEmpty(List<List<string>> groups, int k)
+        {
+            foreach (List<string> group in groups)
+            {
+                Console.Write("| ");
+                foreach (string s in group)
+                {
+                    Console.Write(s + " ");
+                }                
+            }
+            Console.Write("|");
+
+            if (groups.Count < k)
+            {
+                for (int i = 0; i < k - groups.Count; i++)
+                {
+                    Console.Write(" |");
+                }
+            }
+            Console.WriteLine();
+        }
+
     }
 
 }
